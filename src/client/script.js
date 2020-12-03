c394acb2900b486ddb4beaeec6bd09471b9ba240
Bricktech2000 <bricktech2000@gmail.com>
2020-12-03 11:22:41 -0500

    added GPUinfo logs

--- src/client/script.js
+++ src/client/script.js
@@ -1,7 +1,27 @@
 console.log('Running...');
+
 var WebInfo = {
     getData: async function(){
-        return new Promise((resolve, reject) => resolve({key1: 'value1', key2: 'value2'}));
+        var data = {};
+        for(var key of Object.keys(WebInfo.data))
+            data[key] = await WebInfo.data[key];
+        return data;
     },
+    data: {},
 };
+
+(() => {
+    WebInfo.data['key1'] = 'value1';
+    WebInfo.data['key2'] = 'value2';
+})();
+
+(() => {
+    var canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    var gl = canvas.getContext('experimental-webgl');
+    var debugRendererInfo = gl.getExtension('WEBGL_debug_renderer_info');
+    WebInfo.data['GPUinfo_renderer'] = gl.getParameter(debugRendererInfo.UNMASKED_RENDERER_WEBGL);
+    WebInfo.data['GPUinfo_vendor'] = gl.getParameter(debugRendererInfo.UNMASKED_VENDOR_WEBGL);
+})();
+
 export { WebInfo };
