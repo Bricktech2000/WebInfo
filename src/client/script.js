517b09cc81efa9850f320a48eac71923eff9316e
Bricktech2000 <bricktech2000@gmail.com>
2020-12-03 12:15:10 -0500

    added a few more logs and restructured getData

--- src/client/script.js
+++ src/client/script.js
@@ -4,27 +4,50 @@ console.log('Running...');
 var WebInfo = {
     getData: async function(){
         var data = {};
-        for(var key of Object.keys(WebInfo.data))
-            data[key] = await (WebInfo.data[key] || '[undefined]');
+
+        await (async () => { try{
+            data['key1'] = 'value1';
+            data['key2'] = 'value2';
+        }catch(e){console.warn(e)} })();
+        
+        await (async () => { try{
+            //TODO: parse user agent
+            data['general'] = {};
+            data['general'].userAgent = navigator.userAgent || '[empty userAgent]';
+            data['general'].referer = document.referer || '[direct navigation]';
+        }catch(e){console.warn(e)} })();
+        
+        await (async () => { try{
+            data['screen'] = {};
+            data['screen'].width = `${window.screen.width} pixels`;
+            data['screen'].height = `${window.screen.height} pixels`;
+            data['screen'].pixelDepth = `${window.screen.pixelDepth} bits / pixel`;
+        }catch(e){console.warn(e)} })();
+        
+        await (async () => { try{
+            var canvas = document.createElement('canvas');
+            document.body.appendChild(canvas);
+            var gl = canvas.getContext('experimental-webgl');
+            var debugRendererInfo = gl.getExtension('WEBGL_debug_renderer_info');
+            data['GPU'] = {};
+            data['GPU'].renderer = gl.getParameter(debugRendererInfo.UNMASKED_RENDERER_WEBGL) || '[unknown renderer]';
+            data['GPU'].vendor = gl.getParameter(debugRendererInfo.UNMASKED_VENDOR_WEBGL) || '[unknown vendor]';
+        }catch(e){console.warn(e)} })();
+        
+        await (async () => { try{
+            try{
+                data['battery'] = {};
+                var batteryInfo = await navigator.getBattery();
+                data['battery']['charging'] = batteryInfo.charging ? 'yes' : 'no';
+                data['battery']['level'] = `${batteryInfo.level * 100}%`;
+            }catch(e){
+                data['battery'] = '[no battery or access denied]';
+            }
+        }catch(e){console.warn(e)} })();
+
         return data;
-    },
-    data: {},
+    }
 };
-
-(() => {
-    WebInfo.data['key1'] = 'value1';
-    WebInfo.data['key2'] = 'value2';
-})();
-
-(() => {
-    var canvas = document.createElement('canvas');
-    document.body.appendChild(canvas);
-    var gl = canvas.getContext('experimental-webgl');
-    var debugRendererInfo = gl.getExtension('WEBGL_debug_renderer_info');
-    WebInfo.data['GPU'] = {};
-    WebInfo.data['GPU'].renderer = gl.getParameter(debugRendererInfo.UNMASKED_RENDERER_WEBGL) || '[unknown renderer]';
-    WebInfo.data['GPU'].vendor = gl.getParameter(debugRendererInfo.UNMASKED_VENDOR_WEBGL) || '[unknown vendor]';
-})();
 
 
 export { WebInfo };
