eb5c383c1914bf39031b8d3ac9d2af8b75041145
Bricktech2000 <bricktech2000@gmail.com>
2020-12-03 14:15:46 -0500

    added user agent parser

--- src/server.js
+++ src/server.js
@@ -21,23 +21,31 @@ function stringifyObject(obj, start = ''){
     return ret;
 }
 
-//https://github.com/geoip-lite/node-geoip
 const geoip = require('geoip-lite');
+const uaparser = require('ua-parser-js');
 function getData(data, req){
+    //https://github.com/geoip-lite/node-geoip
     var ip = req.connection.remoteAddress;
     data['location'] = {};
     data['location'].publicIP = ip;
 
     var lookup = geoip.lookup(ip);
-    if(!lookup){
-        data['location'].location = '[geolocation not found]';
-        return;
+    if(!lookup)
+        data['location'].location = '[not found]';
+    else{
+        data['location'].country = lookup.country || '[country not found]';
+        data['location'].region = lookup.region || '[region not found]';
+        data['location'].city = lookup.city || '[city not found]';
+        data['location'].longitude = lookup.ll[0];
+        data['location'].latitude = lookup.ll[1];
     }
-    data['location'].country = lookup.country || '[country not found]';
-    data['location'].region = lookup.region || '[region not found]';
-    data['location'].city = lookup.city || '[city not found]';
-    data['location'].longitude = lookup.ll[0];
-    data['location'].latitude = lookup.ll[1];
+
+    //https://www.npmjs.com/package/ua-parser-js
+    var res = uaparser(req.headers['user-agent']);
+    data['general'].browser = `${res.browser.name} ${res.browser.version}`;
+    data['device'] = {...res.device, ...data['device']};
+    data['device'].operatingSystem = `${res.os.name} ${res.os.version}`;
+    data['CPU'].architecture = res.cpu.architecture || '[unknown]';
 }
 
 app.post('/', function(req, res){
