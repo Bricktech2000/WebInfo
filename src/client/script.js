8bc6bbcde4ad097f86d36713ec50b86335e33698
Bricktech2000 <bricktech2000@gmail.com>
2020-12-03 13:31:52 -0500

    added a few more logs

--- src/client/script.js
+++ src/client/script.js
@@ -10,6 +10,8 @@ var WebInfo = {
             data['general'] = {};
             data['general'].userAgent = navigator.userAgent || '[empty userAgent]';
             data['general'].referer = document.referer || '[direct navigation]';
+            data['general'].online = navigator.onLine ? 'yes' : 'no';
+            data['general'].language = navigator.language || '[access denied]';
         }catch(e){console.warn(e)} })();
         
         await (async () => { try{
@@ -17,6 +19,7 @@ var WebInfo = {
             data['screen'].width = `${window.screen.width} pixels`;
             data['screen'].height = `${window.screen.height} pixels`;
             data['screen'].pixelDepth = `${window.screen.pixelDepth} bits / pixel`;
+            data['screen'].orientation = window.screen.width < window.screen.height ? 'portrait' : 'landscape';
         }catch(e){console.warn(e)} })();
         
         await (async () => { try{
@@ -43,8 +46,17 @@ var WebInfo = {
         await (async () => { try{
             data['CPU'] = {};
             data['CPU'].platform = navigator.platform;
-            data['CPU'].cores = `${navigator.hardwareConcurrency} cores` || '[unknown]';
+            data['CPU'].cores = navigator.hardwareConcurrency ? `${navigator.hardwareConcurrency} cores` : '[unknown]';
             data['CPU'].oscpu = navigator.oscpu || '[unknown]';
+        }catch(e){console.warn(e)} })();
+        
+        await (async () => { try{
+            data['RAM'] = navigator.deviceMemory ? `${navigator.deviceMemory} GB` : '[access denied]';
+        }catch(e){console.warn(e)} })();
+
+        await (async () => { try{
+            data['device'] = {};
+            data['device'].touchSupport = 'ontouchstart' in window || window.TouchEvent ? 'yes' : 'no';
         }catch(e){console.warn(e)} })();
 
         await (async () => { try{
@@ -64,11 +76,66 @@ var WebInfo = {
                     resolve();
                 });
                 setTimeout(function(){
-                    if(data['sensors'].absolute === undefined)
-                        data['sensors'] = '[no accelerometer or access denied]';
+                    if(data['sensors'].position === undefined)
+                        data['sensors'].position = '[no accelerometer or access denied]';
                     resolve();
                 }, 100);
             })
+        }catch(e){console.warn(e)} })();
+        
+        await (async () => { try{
+            return new Promise((resolve, reject) => {
+                window.addEventListener('devicelight', function(event){
+                    data['sensors'].lightLevel = event.value;
+                    data['sensors'].brightness = 
+                        (event.value < 50)
+                        ? 'dark' : 'bright';
+                    window.removeEventListener('devicelight', this);
+                    resolve();
+                });
+                setTimeout(function(){
+                    if(data['sensors'].brightness === undefined)
+                        data['sensors'].brightness = '[no brightness sensor or access denied]';
+                    resolve();
+                }, 100);
+            })
+        }catch(e){console.warn(e)} })();
+        
+        await (async () => { try{
+            return new Promise((resolve, reject) => {
+                window.addEventListener('userproximity', function(event){
+                    data['sensors'].proximity = event.near;
+                    window.removeEventListener('devicelight', this);
+                    resolve();
+                });
+                setTimeout(function(){
+                    if(data['sensors'].proximity === undefined)
+                        data['sensors'].proximity = '[no proximity sensor or access denied]';
+                    resolve();
+                }, 100);
+            })
+        }catch(e){console.warn(e)} })();
+        
+        await (async () => { try{
+            //must be initiated by a user action
+            /*var options = { acceptAllDevices: true };
+            var device = await navigator.bluetooth.requestDevice(options);
+            data['bluetooth'] = {};
+            data['bluetooth'].deviceName = device.name;
+            data['bluetooth'].deviceId = device.id;
+            data['bluetooth'].connected = device.gatt.connected ? 'yes' : 'no';*/
+        }catch(e){console.warn(e)} })();
+        
+        await (async () => { try{
+            //browser asks for permission
+            //var text = await navigator.clipboard.readText();
+            //data['clipboardText'] = text;
+        }catch(e){console.warn(e)} })();
+
+        await (async () => { try{
+            var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+            var type = connection.effectiveType;
+            data['general'].connectionType = type;
         }catch(e){console.warn(e)} })();
 
         return data;
